97bd3840272129b308138c73cbee00075866ec25
ff2400t <84376771+ff2400t@users.noreply.github.com>
2022-09-14 18:34:00 +0530

    Now the order for dest and src doesn't matter, it will work regardless

--- main.js
+++ main.js
@@ -1,92 +1,100 @@
 import "./style.css";
-import 'dropzone/dist/dropzone.css';
+import "dropzone/dist/dropzone.css";
 import Dropzone from "dropzone";
 
-const main = document.querySelector('main');
-const clearBtn = document.querySelector('.clear-btn');
-const worker = new Worker(new URL('./worker.js', import.meta.url));
-let state;
+const main = document.querySelector("main");
+const clearBtn = document.querySelector(".clear-btn");
+const worker = new Worker(new URL("./worker.js", import.meta.url));
+let state = {};
 
-const dest = new Dropzone('#dest', {
-  acceptedFiles: 'image/*',
-  dictDefaultMessage: 'Alpha Destination Image',
+const dest = new Dropzone("#dest", {
+  acceptedFiles: "image/*",
+  dictDefaultMessage: "Alpha Destination Image",
   autoProcessQueue: false,
   maxFiles: 1,
 });
 
-const src = new Dropzone('#src', {
-  acceptedFiles: 'image/*',
-  dictDefaultMessage: 'Alpha Source Image',
+const src = new Dropzone("#src", {
+  acceptedFiles: "image/*",
+  dictDefaultMessage: "Alpha Source Image",
   autoProcessQueue: false,
   maxFiles: 1,
 });
 
-dest.on('addedfile', async (file) => {
-  state = await createImageBitmap(file)
-    .then((bitmap) => ({
-      height: bitmap.height,
-      width: bitmap.width,
-      //ImageData
-      dest: getImageDataFromBitmap(bitmap)
-    }))
+dest.on("addedfile", async (file) => {
+  state.dest = file;
   sendData();
-})
+});
 
-src.on('addedfile', async (file) => {
-  // ImageData
-  state.src = await createImageBitmap(file, {
-    resizeWidth: state.width,
-    resizeHeight: state.height,
-    resizeQuality: "high"
-  })
-    .then(getImageDataFromBitmap)
+src.on("addedfile", async (file) => {
+  state.src = file;
   sendData();
-})
+});
 
-function sendData() {
-  if (state.src && state.dest){
-  const message = {
-    src: state.src.data,
-    dest: state.dest.data,
-  }
-  worker.postMessage(message, [state.src.data.buffer, state.dest.data.buffer]) 
+async function sendData() {
+  if (
+    state.hasOwnProperty("src") &&
+    state.hasOwnProperty("dest")
+  ) {
+    const destBitmap = await createImageBitmap(state.dest);
+    const { height, width } = destBitmap;
+    state.height = height;
+    state.width = width;
+    const dest = getImageDataFromBitmap(destBitmap);
+
+    const srcBitmap = await createImageBitmap(state.src, {
+      resizeWidth: width,
+      resizeHeight: height,
+      resizeQuality: "high",
+    });
+    const src = getImageDataFromBitmap(srcBitmap);
+
+    const message = {
+      src: src.data,
+      dest: dest.data,
+    };
+
+    worker.postMessage(
+      message,
+      [src.data.buffer, dest.data.buffer],
+    );
   }
 }
 
 worker.onmessage = (e) => {
   const { height: h, width: w } = state;
-  const dataArray = new Uint8ClampedArray(e.data.result)
-  const newImage = new ImageData(dataArray, w, h)
+  const dataArray = new Uint8ClampedArray(e.data.result);
+  const newImage = new ImageData(dataArray, w, h);
 
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
   canvas.width = w;
   canvas.height = h;
-  ctx.putImageData(newImage, 0, 0); 
+  ctx.putImageData(newImage, 0, 0);
   const url = canvas.toDataURL();
 
   addResultAndLink(url);
-}
+};
 
 function addResultAndLink(imageSrc) {
-  const div = document.createElement('div');
-  div.className = 'result-div'
+  const div = document.createElement("div");
+  div.className = "result-div";
   div.innerHTML =
     `<a href='${imageSrc}' download='image-overlay.png' class='download'>Download</a>
-  <img src='${imageSrc}' class='alpha-overlay'/>`
+  <img src='${imageSrc}' class='alpha-overlay'/>`;
   main.appendChild(div);
 }
 
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   dest.removeAllFiles();
   src.removeAllFiles();
-  const results = document.querySelectorAll('.result-div');
-  if (results) results.forEach(e => e.remove());
-})
+  const results = document.querySelectorAll(".result-div");
+  if (results) results.forEach((e) => e.remove());
+});
 
 function getImageDataFromBitmap(bitmap) {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
   const { height: h, width: w } = bitmap;
 
   canvas.width = w;
--- worker.js
+++ worker.js
@@ -1,13 +1,15 @@
 // rewrite alpha channel from arr1 to arr2
 function replaceAlpha(src, dest) {
-	if (src.length !== dest.length) throw Error("Array size length should be equal");
-	for (let i = 3; i < src.length; i += 4) {
-		dest[i] = src[i]
-	}
-	return dest
+  if (src.length !== dest.length) {
+    throw Error("Array size length should be equal");
+  }
+  for (let i = 3; i < src.length; i += 4) {
+    dest[i] = src[i];
+  }
+  return dest;
 }
 
 onmessage = (e) => {
-	const result = replaceAlpha(e.data.src, e.data.dest);
-	postMessage({ result }, [result.buffer])
-}
+  const result = replaceAlpha(e.data.src, e.data.dest);
+  postMessage({ result }, [result.buffer]);
+};
